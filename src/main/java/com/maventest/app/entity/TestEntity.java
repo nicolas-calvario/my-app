5a3a12cb110cf614172e6ee43e38287a9e81f0b1
Nicolas Calvario <nicolas.calvario.gonzalez@gmail.com>
2024-10-12 14:39:00 -0600

    iniciando el proyecto de maven de prueba

---
+++ src/main/java/com/maventest/app/entity/TestEntity.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.maventest.app.entity;
+
+/**
+ * 
+ */
+public class TestEntity {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
